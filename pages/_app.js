b7cc4670503c7852aecdb21ec4b3c2035cf7a094
bcthakre <bcthakregcp@gmail.com>
2022-10-09 10:45:44 -0500

    Added signIn and signOut

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import { SessionProvider } from 'next-auth/react';
+import { ChakraProvider } from '@chakra-ui/react';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+  return (
+    <SessionProvider session={session}>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </SessionProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
